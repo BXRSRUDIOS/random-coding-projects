b225dcc05ee274de1727f419f883aa4db3340ab8
BXRSRUDIOS <bilalrajastrikes23@gmail.com>
2024-12-07 21:19:12 +0000

    Initialise Player Movement

--- Shooter Game/classes.py
+++ Shooter Game/classes.py
@@ -4,13 +4,34 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
+    def __init__(self, x, y, scale, velocity, char_type):
         # Temporary Placeholder image for all entities
         super().__init__()
+        self.velocity = velocity
+        self.direction = 1
+        self.flip = False
+        self.char_type = char_type
         img = pygame.image.load("Shooter Game/assets/MAIN_CHARACTER.png")
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_width() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+
+        if moving_left:
+            dx = -self.velocity
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.velocity
+            self.flip = False
+            self.direction = 1
+
+        self.rect.x += dx  
+        self.rect.y += dy
+    
     def draw(self):
-        screen.blit(self.image, self.rect)
+        # Just draw the image so I don't have to repeat this line of code
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
--- Shooter Game/main.py
+++ Shooter Game/main.py
@@ -2,29 +2,61 @@ import pygame
 import classes
 pygame.init()
 
+# Initialise screen
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Random Shooter Game")
 
+# Any random variables go here
 running = True
+moving_left = False
+moving_right = False
 
+# Initialise background
+BG = (144, 201, 120)
+screen.fill(BG)
+
+# Game FPS for Game Loop
+clock = pygame.time.Clock()
+FPS = 60
 
 # Initialise player boilerplate code
 x = 250
 y = 250
 scale = 0.125/2
 
-player = classes.Entity(x, y, scale)
-
+player = classes.Entity(x, y, scale, 2, "player")
+running = True
 while running:
 
-    player.draw()
+    clock.tick(FPS)
 
-    for event in pygame.event.get(): # quit the game
+    screen.fill(BG)
+
+    player.draw()
+    player.move(moving_left, moving_right)
+
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                running = False
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+	
+    
 
     pygame.display.update()
 
